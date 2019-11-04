e4bc442b41fb6432f136d77ed72a5e2a564671d6
Edwin Cubillos <edwin.cubillos.ext@nokia.com>
2019-11-04 17:15:24 -0300

    develop :lipstick: styled-component  :construction: app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './App'
 
-
-ReactDOM.render(<strong> hello world react </strong>, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
